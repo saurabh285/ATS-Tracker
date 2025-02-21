c24458e0ed4f8bd3fb0a6458cd0ce0a78ba044eb
Saurabh Singh <saurabh241singh@gmail.com>
2025-02-22 00:41:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def get_gemini_response(input,pdf_cotent,prompt):
     model=genai.GenerativeModel('gemini-1.5-flash')
